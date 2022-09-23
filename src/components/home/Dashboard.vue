41d6bca5dfbe9ccb1d73e4a4934413ae84d711c2
Rovian Vieceli <rovianvieceli@gmail.com>
2022-09-23 13:49:17 -0300

    Adicionado arquivo README para instruções básicas

---
+++ src/components/home/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="hello">
+    Começe alterando aqui!
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {}
+})
+export default class Hello extends Vue {
+}
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home">
+    <Dashboard/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Dashboard from "@/components/home/Dashboard.vue";
+
+@Component({
+  components: {Dashboard},
+})
+export default class Home extends Vue {
+}
+</script>
